9e2379cef0a6522fb3d65c3ee3918e2ca82e5a81
UK-devs <156414810+UK-devs@users.noreply.github.com>
2024-04-01 23:24:08 +0800

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(cors(
     {
-    origin: 'http://localhost:4200',
+    origin: 'https://www.hau-oces.com',
     credentials: true
 }
 ));
